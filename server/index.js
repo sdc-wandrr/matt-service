46533cd62e8ecfb79f8c0133b60d00051acee75d
Evan Kolb <kongaiwen@gmail.com>
2020-08-05 17:20:54 -0700

    Write API endpoint, model method and query

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 let port = 3000;
+let models = require('./models/index.js');
 
 app.use(express.json());
 app.use(express.static(__dirname + '../client'));
 
 
 
-
+app.get('/api/images', (req, res) => {
+  // invoke images model method "fetchAllByHostel"
+  let hostelId = req.params.id;
+  models.image.fetchAllByHostelId(hostelId, (error, images) => {
+    if (error) {
+      res.status(502).send();
+      return;
+    }
+    res.send(images);
+  });
+});
 
 
 
---
+++ server/models/images.js
@@ -0,0 +1,26 @@
+let db = require('../database/index.js');
+
+class Image {
+
+  fetchAllByHostelId (id, callback) {
+    let queryStr = 'SELECT * FROM `images` WHERE hostel_id = ?';
+    let queryArgs = [id];
+
+    db.query(queryStr, queryArgs, (error, results, fields) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      callback(null, results);
+    })
+  }
+}
+
+
+
+
+
+
+
+
+module.exports = new Image();
