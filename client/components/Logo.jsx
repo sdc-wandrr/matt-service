dbf73ee6ad23972fce6d542ea7daa0bf6df8f669
Evan Kolb <kongaiwen@gmail.com>
2020-08-11 07:19:05 -0700

    Fix logo

--- client/components/Logo.jsx
+++ client/components/Logo.jsx
@@ -6,19 +6,44 @@ const StyledLogo = styled.div`
   grid-column: 1 / 2;
   grid-row: 1 / 2;
   color: #fff;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 5px;
+  display: grid;
+  grid-template-columns: 75px 1fr;
+  grid-template-rows: 1fr;
   gap: 5px;
+  margin: 10px;
+`;
+
+const StyledLogoDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
+  z-index: 5;
+`;
+
+const Backsplash = styled.div`
+  width: 90%;
+  height: 90%;
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
+  background-color: #fff;
+  z-index: 0;
+  margin: 4px 0 0 4px;
+  border-radius: 25px;
+`;
+
+const StyledTitle = styled.h1`
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
 `;
 
 const Logo = (props) => (
   <StyledLogo>
-    <LogoIcon fill="#f25621" width="100%" height="100%" viewBox="0 0 450 450" preserveAspectRatio="xMidYMid meet" />
-    <h1>HOSTELWORLD</h1>
+    <StyledLogoDiv>
+      <LogoIcon fill="#f25621" width="100%" height="100%" viewBox="0 0 450 450" preserveAspectRatio="xMidYMid meet" />
+    </StyledLogoDiv>
+    <Backsplash />
+    <StyledTitle>HOSTILEWORLD</StyledTitle>
   </StyledLogo>
 );
 
