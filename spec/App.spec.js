3dc74630246591bcc876e5d277f41141d8ee4ca7
Evan Kolb <kongaiwen@gmail.com>
2020-08-12 09:26:41 -0700

    save

---
+++ spec/App.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import mockData from './mockData.js';
+
+import App from '../client/components/App.jsx';
+
+describe('<App />', () => {
+  test('fetchImagesByHostelId', () => {
+
+  });
+});
--- spec/mockData.js
+++ spec/mockData.js
@@ -20,6 +20,27 @@ const images = [
     description: 'Try to generate the COM interface, maybe it will parse the optical transmitter!',
     hostel_id: 1,
   },
+  {
+    id: 2,
+    file_name: 'img_444.jpg',
+    url: 'https://hostileworldimages.s3.us-east-2.amazonaws.com/img_14.jpg',
+    description: 'Try to generate the COM interface, maybe it will parse the optical transmitter!',
+    hostel_id: 100,
+  },
+  {
+    id: 10,
+    file_name: 'img_56.jpg',
+    url: 'https://hostileworldimages.s3.us-east-2.amazonaws.com/img_14.jpg',
+    description: 'Try to generate the COM interface, maybe it will parse the optical transmitter!',
+    hostel_id: 100,
+  },
+  {
+    id: 5,
+    file_name: 'img_89.jpg',
+    url: 'https://hostileworldimages.s3.us-east-2.amazonaws.com/img_14.jpg',
+    description: 'Try to generate the COM interface, maybe it will parse the optical transmitter!',
+    hostel_id: 1,
+  },
 ];
 
 module.exports = images;
