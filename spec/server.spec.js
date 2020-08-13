ea2c8eeab6246ecf09919840bf4095b8ba427a6b
Evan Kolb <kongaiwen@gmail.com>
2020-08-13 06:13:07 -0700

    add a console.log to server.spec

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -7,6 +7,7 @@ test('GETs the endpoint for images of a hostel', async (done) => {
   const hostelId = 4;
   const res = await request.get(`/api/hostels/${hostelId}/images`);
 
+  console.log(res);
   expect(res.body.length).toBeGreaterThan(0);
   expect(res.body[0].url).toBeDefined();
   done();
