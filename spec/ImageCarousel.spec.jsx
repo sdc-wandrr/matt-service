2334e2d8390c06cd2d9a1a7cf413df2cda0f1eca
Evan Kolb <kongaiwen@gmail.com>
2020-08-12 18:04:34 -0700

    Write more test coverage for image carousel component

--- spec/ImageCarousel.spec.jsx
+++ spec/ImageCarousel.spec.jsx
@@ -4,6 +4,7 @@ import images from './mockData';
 
 import { ImageCarousel, StyledImageCarousel, StyledLeftSidebar, StyledRightSidebar, StyledCurrentImage, StyledCornerGrid, StyledImageCount, StyledBackButton, StyledPricesButton } from '../client/components/ImageCarousel';
 import { ThumbnailCarousel, StyledImage } from '../client/components/ThumbnailCarousel';
+import { ImageGridItem } from '../client/components/ImageGridItem.jsx';
 
 describe('<ImageCarousel />', () => {
   test('handleRightClick function', () => {
@@ -13,11 +14,31 @@ describe('<ImageCarousel />', () => {
     expect(wrapper.state().currentIndex).toBe(1);
   });
 
+  test('handleRightClick function at last image', () => {
+    const dummyFn = () => {};
+    const wrapper = mount(<ImageCarousel images={images} onExit={dummyFn} />);
+    wrapper.find(StyledRightSidebar).simulate('click');
+    wrapper.find(StyledRightSidebar).simulate('click');
+    wrapper.find(StyledRightSidebar).simulate('click');
+    wrapper.find(StyledRightSidebar).simulate('click');
+    wrapper.find(StyledRightSidebar).simulate('click');
+    wrapper.find(StyledRightSidebar).simulate('click');
+    wrapper.find(StyledRightSidebar).simulate('click');
+    expect(wrapper.state().currentIndex).toBe(5);
+  });
+
   test('handleLeftClick function', () => {
     const dummyFn = () => {};
     const wrapper = mount(<ImageCarousel images={images} onExit={dummyFn} />);
     wrapper.find(StyledRightSidebar).simulate('click');
     expect(wrapper.state().currentIndex).toBe(1);
+    wrapper.find(StyledLeftSidebar).simulate('click');
+    expect(wrapper.state().currentIndex).toBe(0);
+  });
+
+  test('handleLeftClick function at first image', () => {
+    const dummyFn = () => {};
+    const wrapper = mount(<ImageCarousel images={images} onExit={dummyFn} />);
     wrapper.find(StyledLeftSidebar).simulate('click');
     expect(wrapper.state().currentIndex).toBe(0);
   });
