a23791279eaa2d1b1afa6b994e1275c744846da5
Evan Kolb <kongaiwen@gmail.com>
2020-08-13 05:50:54 -0700

    Create dummyData sql file

---
+++ server/database/dummyData.sql
@@ -0,0 +1,20 @@
+INSERT INTO hostels (name, address) VALUES ('hostel_1', '50 Grey Avenue');
+INSERT INTO hostels (name, address) VALUES ('hostel_2', '50 Grey Avenue');
+INSERT INTO hostels (name, address) VALUES ('hostel_3', '50 Grey Avenue');
+INSERT INTO hostels (name, address) VALUES ('hostel_4', '50 Grey Avenue');
+INSERT INTO hostels (name, address) VALUES ('hostel_5', '50 Grey Avenue');
+INSERT INTO hostels (name, address) VALUES ('hostel_6', '50 Grey Avenue');
+INSERT INTO hostels (name, address) VALUES ('hostel_7', '50 Grey Avenue');
+
+
+INSERT INTO `images` (file_name, url, description, hostel_id) VALUES ('img_444.jpg', 'https://hostileworldimages.s3.us-east-2.amazonaws.com/img_14.jpg', ' desc', '1');
+INSERT INTO `images` (file_name, url, description, hostel_id) VALUES ('img_444.jpg', 'https://hostileworldimages.s3.us-east-2.amazonaws.com/img_14.jpg', ' desc', '1');
+INSERT INTO `images` (file_name, url, description, hostel_id) VALUES ('img_444.jpg', 'https://hostileworldimages.s3.us-east-2.amazonaws.com/img_14.jpg', ' desc', '2');
+INSERT INTO `images` (file_name, url, description, hostel_id) VALUES ('img_444.jpg', 'https://hostileworldimages.s3.us-east-2.amazonaws.com/img_14.jpg', ' desc', '2');
+INSERT INTO `images` (file_name, url, description, hostel_id) VALUES ('img_444.jpg', 'https://hostileworldimages.s3.us-east-2.amazonaws.com/img_14.jpg', ' desc', '3');
+INSERT INTO `images` (file_name, url, description, hostel_id) VALUES ('img_444.jpg', 'https://hostileworldimages.s3.us-east-2.amazonaws.com/img_14.jpg', ' desc', '3');
+INSERT INTO `images` (file_name, url, description, hostel_id) VALUES ('img_444.jpg', 'https://hostileworldimages.s3.us-east-2.amazonaws.com/img_14.jpg', ' desc', '3');
+
+
+
+
