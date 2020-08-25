8d5d5c570c8b32faa80b19838cedd4ecb8de429a
Evan Kolb <kongaiwen@gmail.com>
2020-08-25 15:38:29 -0700

    Update index.js

--- server/database/index.js
+++ server/database/index.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'traveler',
   password: 'travel',
-  database: 'hostile_world',
+  database: 'image-carousel',
 });
 
 connection.connect();
