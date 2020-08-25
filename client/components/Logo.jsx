ee560c35bef0db19be920f297e7003bbd213ee0f
Evan Kolb <kongaiwen@gmail.com>
2020-08-25 12:35:23 -0700

    Update Logo.jsx

--- client/components/Logo.jsx
+++ client/components/Logo.jsx
@@ -48,7 +48,7 @@ const Logo = (props) => (
       <LogoIcon fill="#f25621" width="100%" height="100%" viewBox="0 0 450 450" preserveAspectRatio="xMidYMid meet" />
     </StyledLogoDiv>
     <Backsplash />
-    <StyledTitle>HOSTILEWORLD</StyledTitle>
+    <StyledTitle>WANDRR</StyledTitle>
   </StyledLogo>
 );
 
