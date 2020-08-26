f346366b7b87b404649d4bbf364086403b8ffe09
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-08-26 16:16:55 -0700

    Implements README & Screenshots.
    - Also adds config files and placeholder config file.

---
+++ server/database/RENAME_ME_TO_CONFIG.js
@@ -0,0 +1,8 @@
+// Add your MySQL user and password info here
+// Don't forget to rename the file 'config.js'
+module.exports = {
+  host: 'localhost',
+  user: 'USERNAME-HERE',
+  password: 'PASSWORD-HERE',
+  database: 'image_carousel',
+};
