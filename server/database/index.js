c8e98f120644f4b80037173e3c9f02f667bf5e45
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-08-27 22:30:14 -0700

    Removes server and database files

--- server/database/index.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const mysqlConfig = require('./config.js');
-
-const connection = mysql.createConnection(mysqlConfig);
-
-connection.connect();
-
-module.exports = connection;
